3e1e3a6d3b163986be35692425c0e42badfa3fc2
Jose Meireles <jmeirele@c1r2s6.42porto.com>
2025-01-06 15:10:22 +0000

    added another protection for .ber extension

--- src/parsing/map_name_checker.c
+++ src/parsing/map_name_checker.c
@@ -6,7 +6,7 @@
 /*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 11:11:17 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/01/02 16:09:22 by jmeirele         ###   ########.fr       */
+/*   Updated: 2025/01/06 10:51:56 by jmeirele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 void	map_name_checker(t_game *game, t_map *map)
 {
 	char	*extension;
+	char	*to_compare;
 
 	extension = ft_strrchr(map->map_name, '.');
-	if (extension && ft_strcmp(extension, ".ber") == 0)
+	to_compare = ft_strrchr(map->map_name, '/');
+	if (extension && (ft_strcmp(extension, ".ber") == 0)
+		&& (ft_strlen(to_compare) > 5))
 		return ;
 	exit_program(game, "Error\nMap name invalid\n", 2);
 }
--- src_bonus/parsing/map_name_checker_bonus.c
+++ src_bonus/parsing/map_name_checker_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   map_name_checker.c                                 :+:      :+:    :+:   */
+/*   map_name_checker_bonus.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 11:11:17 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/01/02 16:11:52 by jmeirele         ###   ########.fr       */
+/*   Updated: 2025/01/06 10:52:04 by jmeirele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 void	map_name_checker(t_game *game, t_map *map)
 {
 	char	*extension;
+	char	*to_compare;
 
 	extension = ft_strrchr(map->map_name, '.');
-	if (extension && ft_strcmp(extension, ".ber") == 0)
+	to_compare = ft_strrchr(map->map_name, '/');
+	if (extension && (ft_strcmp(extension, ".ber") == 0)
+		&& (ft_strlen(to_compare) > 5))
 		return ;
 	exit_program(game, "Error\nMap name invalid\n", 2);
 }
